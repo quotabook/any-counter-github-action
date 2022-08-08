64f540ef3aa3e22a5203204a76fea992fe3b408c
evan-moon <bboydart91@gmail.com>
2022-08-08 20:01:55 +0900

    chore: add info log

--- src/utils/slack/messages/anyCountMessage.ts
+++ src/utils/slack/messages/anyCountMessage.ts
@@ -1,3 +1,4 @@
+import * as core from '@actions/core';
 import { sumBy } from '@quotalab/utils';
 import { getRepository } from '@quotalab/github-action';
 import { WebhookPayload } from '@actions/github/lib/interfaces';
@@ -23,11 +24,18 @@ export async function getLastAnyCount() {
 
 export async function sendAnyCountMessage(payload: WebhookPayload) {
   const repository = getRepository(payload);
+  core.info(`get repository => ${repository?.name}`);
+
   const results = await getAnyErrors();
-  const lastAnyCount = await getLastAnyCount();
   const newAnyCount = sumBy(results, ({ errorCount }) => errorCount);
+  core.info(`get current any count => ${newAnyCount}`);
+
+  const lastAnyCount = await getLastAnyCount();
+  core.info(`get last any count => ${lastAnyCount}`);
 
   const isIncreasedAnyType = newAnyCount > lastAnyCount;
+  core.info(`isIncreased? => ${isIncreasedAnyType}`);
+
   if (isIncreasedAnyType === false) {
     return;
   }
@@ -56,14 +64,14 @@ export async function sendAnyCountMessage(payload: WebhookPayload) {
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: `늘어난 Any 타입 개수: *${newAnyCount - lastAnyCount}*`,
+          text: `늘어난 Any 타입 개수: *${newAnyCount - lastAnyCount}개*`,
         },
       },
       {
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: `전체 Any 타입 개수: *${newAnyCount}*`,
+          text: `전체 Any 타입 개수: *${newAnyCount}개*`,
         },
       },
       {
